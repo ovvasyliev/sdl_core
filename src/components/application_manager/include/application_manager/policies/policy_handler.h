35f6016894c10fed00b25b2c42ff048b7ac8cd4b
Ivo Stoilov (GitHub) <istoilov@luxoft.com>
2017-02-21 10:16:45 +0200

    Add logic for GetListOfPermission response processing in policy

--- src/components/application_manager/include/application_manager/policies/policy_handler.h
+++ src/components/application_manager/include/application_manager/policies/policy_handler.h
@@ -576,9 +576,6 @@ class PolicyHandler : public PolicyHandlerInterface,
 #endif  // EXTERNAL_PROPRIETARY_MODE
   bool SaveSnapshot(const BinaryMessage& pt_string, std::string& snap_path);
 
- private:
-  static const std::string kLibrary;
-
   /**
    * @brief Collects permissions for all currently registered applications on
    * all devices
@@ -595,6 +592,9 @@ class PolicyHandler : public PolicyHandlerInterface,
    */
   std::vector<FunctionalGroupPermission> CollectAppPermissions(
       const uint32_t connection_key);
+
+ private:
+  static const std::string kLibrary;
 
   /**
  * @brief Collects currently registered applications ids linked to their
