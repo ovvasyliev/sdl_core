a291aec3676d742d18be6ea7e81a32c6f436a645
Ivo Stoilov (GitHub) <istoilov@luxoft.com>
2017-02-27 15:44:49 +0200

    Fix build issues and UT failures

--- src/components/application_manager/src/message_helper/message_helper.cc
+++ src/components/application_manager/src/message_helper/message_helper.cc
@@ -1653,7 +1653,9 @@ void MessageHelper::SendGetUserFriendlyMessageResponse(
 #ifdef EXTERNAL_PROPRIETARY_MODE
 void MessageHelper::SendGetListOfPermissionsResponse(
     const std::vector<policy::FunctionalGroupPermission>& permissions,
+#ifdef EXTERNAL_PROPRIETARY_MODE
     const policy::ExternalConsentStatus& external_consent_status,
+#endif  // EXTERNAL_PROPRIETARY_MODE
     uint32_t correlation_id,
     ApplicationManager& app_mngr) {
   using namespace smart_objects;
@@ -1679,6 +1681,7 @@ void MessageHelper::SendGetListOfPermissionsResponse(
   GroupsAppender groups_appender(allowed_functions_array);
   std::for_each(permissions.begin(), permissions.end(), groups_appender);
 
+#ifdef EXTERNAL_PROPRIETARY_MODE
   const std::string external_consent_status_key = "externalConsentStatus";
   msg_params[external_consent_status_key] = SmartObject(SmartType_Array);
 
@@ -1690,6 +1693,7 @@ void MessageHelper::SendGetListOfPermissionsResponse(
   std::for_each(external_consent_status.begin(),
                 external_consent_status.end(),
                 external_consent_status_appender);
+#endif  // EXTERNAL_PROPRIETARY_MODE
 
   app_mngr.ManageHMICommand(message);
 }
